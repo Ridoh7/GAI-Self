781471e95ede58cba5ef7c161aefb19e41e3b9cb
Ridoh7 <ridohlawal96@gmail.com>
2024-12-05 15:38:43 +0100

    Function exercise

--- GAISelf.py
+++ GAISelf.py
@@ -1,9 +1,47 @@
-import time
-for i in range(1, 11, 4):
-    print(i)
+#Exercise on functions, Easy part
+1
+def say_hello(name):
+  print(f"Hello, {name}!")
+say_hello("Ridoh")
 
-for i in range(10, 0, -1):
-    print(i)
-    time.sleep(1)
+2
+def add_three_numbers (a, b, c):
+  print(f"The sum of the three numbers is: ", a+b+c)
+add_three_numbers(5, 6, 7)
 
-print("Happy New Year")
+3
+def area_of_rectangle(length, breadth):
+  print(f"the area of the rectamgle is: ", length*breadth)
+area_of_rectangle(5, 6)
+
+#Intermediate
+1
+def check_even_odd(number):
+  if number%2==0:
+    print(f"{number} is even number")
+  else:
+    print(f"{number} is odd number")
+check_even_odd(7)
+
+2
+def personalized_greeting(name="Geust", age=0):
+  print(f"Hello, {name}! You are {age} years old.")
+personalized_greeting("Ridoh", 32)
+personalized_greeting()
+
+3
+def simple_calculator(a,b,operation):
+  if operation=="+":
+    return (f"The sum of {a} and {b} is:  {a+b}")
+  elif operation=="-":
+    return (f"The difference between {a} and {b} is: {a-b}" )
+  elif operation=="*":
+    print(F"The product of  {a} and {b} is: {a*b}")
+  elif operation=="/":
+    if b==0:
+      print("Error: Division by zero")
+    else:
+      print(f"The division of {a} and {b} is: {round(a/b,2)}")
+  else:
+    print("Invalid operation")
+simple_calculator(5, 6, "/")
